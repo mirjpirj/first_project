2e333f401158f1bb23c9b11a8b807afe176993a8
localchadmin <localchadmin@dhcp-20-44.public.unibe.ch>
2016-09-24 17:52:47 +0200

    right function for archived posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^all$', views.post_list_all, name='post_list_all'),
-    url(r'^archived$', views.post_list_all, name='post_list_archived'),
+    url(r'^archived$', views.post_list_archived, name='post_list_archived'),
 ]
